4ce1afd09f5fbe0f30f364db5f29cb3dc5576954
cybrd <brysuralta@gmail.com>
2024-07-24 02:06:59 +0800

    update relog returns

--- server/my.guard.ts
+++ server/my.guard.ts
@@ -3,7 +3,7 @@ import { IUser } from './models/user';
 
 export function userGuard(req: Request, res: Response, next: NextFunction) {
   if (!req.user) {
-    res.status(401).send('please relog 1');
+    res.status(401).send('please relog 1111');
   } else {
     next();
   }
@@ -17,9 +17,9 @@ export function supervisorGuard(
   const user = req.user as IUser;
 
   if (!user) {
-    res.status(401).send('please relog 2');
+    res.status(401).send('please relog 2222');
   } else if (user.role !== 'admin' && user.role !== 'supervisor') {
-    res.status(401).send('please relog 3');
+    res.status(401).send('please relog 3333');
   } else {
     next();
   }
@@ -29,9 +29,9 @@ export function adminGuard(req: Request, res: Response, next: NextFunction) {
   const user = req.user as IUser;
 
   if (!user) {
-    res.status(401).send('please relog 4');
+    res.status(401).send('please relog 4444');
   } else if (user.role !== 'admin') {
-    res.status(401).send('please relog 5');
+    res.status(401).send('please relog 5555');
   } else {
     next();
   }
